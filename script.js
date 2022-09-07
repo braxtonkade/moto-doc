20b00132783095fa56b87cacf9c1d51163133587
Braxton Dalton <braxtonkade@gmail.com>
2022-09-07 14:12:42 +0900

    added basic styling to nav li elements. added js functionality to open and close nav on button commands

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const navMenu = document.getElementById('nav-menu');
+
 const navButton = document.getElementById('nav-button');
+
+const navClose = document.getElementById('nav-close');
+
+navButton.addEventListener('click', () => {
+  navMenu.style.display = 'block';
+  navButton.style.display = 'none';
+});
+
+navClose.addEventListener('click', () => {
+  navMenu.style.display = 'none';
+  navButton.style.display = 'block';
+});
