1faf848b21f49b34807a29b58862b48aaca4ec6f
Braxton Dalton <braxtonkade@gmail.com>
2022-09-11 13:24:23 +0900

    restyled nav and established sections added new js functionality for scrolling page

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const navMenu = document.getElementById('nav-menu');
+const nav = document.querySelector('nav');
 
 const navButton = document.getElementById('nav-button');
+const navButtonBg = document.getElementById('nav-button-bg');
 
 const navClose = document.getElementById('nav-close');
 
+const previousScrollPosition = window.scrollY;
+
 navButton.addEventListener('click', () => {
-  navMenu.style.display = 'block';
+  nav.style.top = '0';
   navButton.style.display = 'none';
 });
 
 navClose.addEventListener('click', () => {
-  navMenu.style.display = 'none';
+  nav.style.top = '-100vw';
   navButton.style.display = 'block';
 });
+
+window.onscroll = function () {
+  let currentScrollPosition = window.scrollY;
+  if (currentScrollPosition === previousScrollPosition) {
+    navButtonBg.style.top = '-5rem';
+  } else {
+    navButtonBg.style.top = '0';
+  }
+};
