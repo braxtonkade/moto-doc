f6f8df9f46bf681ff24c25a8c536bd0baf0541e7
Braxton Dalton <braxtonkade@gmail.com>
2022-09-22 00:37:37 -0800

    built and designed functional testimonial card slider. built and designed footer section

--- script.js
+++ script.js
@@ -11,10 +11,9 @@ if (history.scrollRestoration) {
   history.scrollRestoration = 'manual';
 } else {
   window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
   };
 }
-
 
 navButton.addEventListener('click', () => {
   nav.style.top = '0';
@@ -35,4 +34,67 @@ window.onscroll = function () {
   }
 };
 
+// test for carousel
 
+var isAnimating = false;
+
+function scrollLeftAnimate(elem, unit) {
+  if (!elem || isAnimating) {
+    //if element not found / if animating, do not execute slide
+    return;
+  }
+
+  var time = 300; // animation duration in MS, the smaller the faster.
+  var from = elem.scrollLeft; // to continue the frame posistion
+  var aframe = 10; //fraction of frame frequency , set 1 for smoothest  ~ set 10++ for lower FPS (reduce CPU usage)
+  isAnimating = true; //if animating prevent double trigger animation
+
+  var start = new Date().getTime(),
+    timer = setInterval(function () {
+      var step = Math.min(1, (new Date().getTime() - start) / time);
+      elem.scrollLeft = step * unit + from;
+      if (step === 1) {
+        clearInterval(timer);
+        isAnimating = false;
+      }
+    }, aframe);
+}
+
+function initDealCarrousel(dealCarrouselID) {
+  var target = document.querySelector(
+    '#' + dealCarrouselID + ' .card-container'
+  );
+  var cardOutterWidth;
+  var maxCarrouselScroll;
+
+  function updateUpaCarrouselInfo() {
+    cardOutterWidth = document.querySelector(
+      '#' + dealCarrouselID + ' .card'
+    ).offsetWidth -10; //you can define how far the scroll
+    maxCarrouselScroll =
+      document.querySelectorAll('#' + dealCarrouselID + ' .card').length *
+        cardOutterWidth -
+      document.querySelector('#' + dealCarrouselID + ' .card-container')
+        .clientWidth;
+  }
+
+  document
+    .querySelector('#' + dealCarrouselID + ' .deals-scroll-left')
+    .addEventListener('click', function () {
+      updateUpaCarrouselInfo(); //in case window resized, will get new info
+      if (target.scrollLeft > 0) {
+        scrollLeftAnimate(target, -cardOutterWidth * 2.1);
+      }
+    });
+
+  document
+    .querySelector('#' + dealCarrouselID + ' .deals-scroll-right')
+    .addEventListener('click', function () {
+      updateUpaCarrouselInfo(); //in case window resized, will get new info
+      if (target.scrollLeft < maxCarrouselScroll) {
+        scrollLeftAnimate(target, cardOutterWidth * 2.1);
+      }
+    });
+}
+// Initiate the container with ID
+initDealCarrousel('carousel-container'); //carrousel ID
