0b62632755f68693fb81f69112052b3de04ca65c
Braxton Dalton <braxtonkade@gmail.com>
2022-11-07 10:55:16 +0900

    added dropdown booking functionality with js and styling to services page

--- script.js
+++ script.js
@@ -42,6 +42,8 @@ window.onscroll = function () {
 //   return false;
 // };
 
+// testimonial slider
+
 const slider = document.querySelector('.slides-wrapper'),
   slides = Array.from(document.querySelectorAll('.slide'));
 
@@ -127,67 +129,31 @@ function setPositionByIndex() {
 //   }
 // };
 
-// // test for carousel
+// services
 
-// var isAnimating = false;
+const services = Array.from(document.querySelectorAll('.service')),
+  downArrows = Array.from(document.querySelectorAll('.fa-angle-down'));
 
-// function scrollLeftAnimate(elem, unit) {
-//   if (!elem || isAnimating) {
-//     //if element not found / if animating, do not execute slide
-//     return;
-//   }
+services.forEach((service, index) => {
+  service.addEventListener('click', btnDropdown(index));
+});
 
-//   var time = 300; // animation duration in MS, the smaller the faster.
-//   var from = elem.scrollLeft; // to continue the frame posistion
-//   var aframe = 10; //fraction of frame frequency , set 1 for smoothest  ~ set 10++ for lower FPS (reduce CPU usage)
-//   isAnimating = true; //if animating prevent double trigger animation
 
-//   var start = new Date().getTime(),
-//     timer = setInterval(function () {
-//       var step = Math.min(1, (new Date().getTime() - start) / time);
-//       elem.scrollLeft = step * unit + from;
-//       if (step === 1) {
-//         clearInterval(timer);
-//         isAnimating = false;
-//       }
-//     }, aframe);
-// }
 
-// function initDealCarrousel(dealCarrouselID) {
-//   var target = document.querySelector(
-//     '#' + dealCarrouselID + ' .card-container'
-//   );
-//   var cardOutterWidth;
-//   var maxCarrouselScroll;
+function btnDropdown(index) {
+  let clicked = false;
 
-//   function updateUpaCarrouselInfo() {
-//     cardOutterWidth = document.querySelector(
-//       '#' + dealCarrouselID + ' .card'
-//     ).offsetWidth -10; //you can define how far the scroll
-//     maxCarrouselScroll =
-//       document.querySelectorAll('#' + dealCarrouselID + ' .card').length *
-//         cardOutterWidth -
-//       document.querySelector('#' + dealCarrouselID + ' .card-container')
-//         .clientWidth;
-//   }
-
-//   document
-//     .querySelector('#' + dealCarrouselID + ' .deals-scroll-left')
-//     .addEventListener('click', function () {
-//       updateUpaCarrouselInfo(); //in case window resized, will get new info
-//       if (target.scrollLeft > 0) {
-//         scrollLeftAnimate(target, -cardOutterWidth * 1.1);
-//       }
-//     });
-
-//   document
-//     .querySelector('#' + dealCarrouselID + ' .deals-scroll-right')
-//     .addEventListener('click', function () {
-//       updateUpaCarrouselInfo(); //in case window resized, will get new info
-//       if (target.scrollLeft < maxCarrouselScroll) {
-//         scrollLeftAnimate(target, cardOutterWidth * 1);
-//       }
-//     });
-// }
-// // Initiate the container with ID
-// initDealCarrousel('carousel-container'); //carrousel ID
+  return function (e) {
+    if (clicked === false) {
+      services[index].querySelector('.fa-angle-down').style.transform = 'rotate(-180deg)';
+      services[index].querySelector('.book-service-btn').style.top = '100%';
+      services[index].style.marginBottom = '5rem';
+      clicked = true;
+    } else {
+      services[index].querySelector('.fa-angle-down').style.transform = 'rotate(0)';
+      services[index].querySelector('.book-service-btn').style.top = '55%';
+      services[index].style.marginBottom = '2rem';
+      clicked = false;
+    }
+  };
+}
